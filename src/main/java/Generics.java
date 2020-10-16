a6aeffed80f0093d55287a9885a73f58be084cdb
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-16 19:12:31 +0530

    generics string

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -1,15 +1,15 @@
 public class Generics {
     public static void main(String[] args) {
 
-        System.out.println(findMax(5.1f,8.2f,12.5f));
+        System.out.println(findMax("Banana","apple","mango"));
     }
 
-    public static Float findMax (Float a,Float b,Float c) {
-        Float max=a;
-        if(b.compareTo(max)>0){
+    public static String findMax (String a,String b,String c) {
+        String max=a;
+        if(b.compareToIgnoreCase(max)>0){
             max=b;
         }
-        if(c.compareTo(max)>0){
+        if(c.compareToIgnoreCase(max)>0){
             max=c;
         }
         return max;
--- src/test/java/GenericsTest.java
+++ src/test/java/GenericsTest.java
@@ -5,20 +5,20 @@ public class GenericsTest {
 Generics genObj=new Generics();
 @Test
     public void firstTest(){
-    Float maximum=genObj.findMax(12.5f,4.6f,8.3f);
-        Assert.assertEquals(12.5f,maximum,0.0000);
+    String maximum=genObj.findMax("mango","banana","apple");
+        Assert.assertEquals("mango",maximum);
 
 }
     @Test
     public void secondTest(){
-        Float maximum=genObj.findMax(4.6f,12.5f,8.3f);
-            Assert.assertEquals(12.5f,maximum,0.0000);
+        String maximum=genObj.findMax("banana","mango","apple");
+            Assert.assertEquals("mango",maximum);
 
     }
     @Test
     public void thirdTest(){
-        Float maximum=genObj.findMax(4.6f,8.3f,12.5f);
-            Assert.assertEquals(12.5f,maximum,0.000);
+        String maximum=genObj.findMax("apple","banana","mango");
+            Assert.assertEquals("mango",maximum);
 
     }
 }
